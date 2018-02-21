73f9af02540e158a99cac977faeabdb4ab9260bc
Philipp Reitter <p.reitter@accessio.at>
2018-02-21 10:36:40 +0100

    added virtualfish settings and kname

--- alias.fish
+++ alias.fish
@@ -1,2 +1,8 @@
+# common helpers
+alias apt='sudo apt'
+alias apt-get='sudo apt-get'
+
+# kubernetes helpers
+alias kname='kubectl config set-context (kubectl config current-context) --namespace '
 alias kaf='kubectl apply -f'
 alias k='kubectl'
--- environment.fish
+++ environment.fish
@@ -1,3 +1,8 @@
+# theme settings
 set -g theme_display_k8s_context no
 set -g theme_nerd_fonts yes
+
+# virtualfish settings
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
+set -g VIRTUALFISH_PLUGINS "auto_activation compat_aliases"
+set -x VIRTUALFISH_HOME "$HOME/.virtualenvs"
