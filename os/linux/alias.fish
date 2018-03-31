0190f6d0599bb248f5e68fb0ae049f9606515d71
rettier <rettier@users.noreply.github.com>
2018-03-31 23:58:29 +0200

    Update alias.fish

--- os/linux/alias.fish
+++ os/linux/alias.fish
@@ -1,3 +1,9 @@
-alias apt='sudo apt'
-alias apt-get='sudo apt-get'
+# apt-get 
+abbr -a apt sudo apt
+abbr -a apt-get sudo apt-get
+abbr -a apu sudo apt-get update
+abbr -a api sudo apt-get install
+abbr -a apls sudo apt list --installed
+
+# misc
 alias open='xdg-open'
