d3231c13d28fc9cbcfaae48fbf94f1e17095d960
Philipp Reitter <p.reitter@gmail.com>
2018-07-08 14:25:12 +0200

    added zz macro combining z and fzf

--- functions.fish
+++ functions.fish
@@ -6,6 +6,10 @@ function unix --description 'Converts unix timestamp to human readable datetime'
     date -d @(echo "$argv" | cut -d"." -f1)
 end
 
+function zz --description 'search through and cd into z history with fzf'
+    cd (z -l | sed 's/\([0-9]\+\)\.\([0-9]\+\)/\1,\2/' | sort -n | fzf --tac --no-sort --with-nth 2 --preview "ls --color=always {2}" --preview-window=up -q "$argv" | tr -s "[:space:]" | cut -d" " -f2)
+end
+
 function make
     if test (count $argv) -eq 1 ; and [ "$argv" = "sandwich" ]
         echo "H4sIAIEPvloA/2VQwU7EIBS88xXPxsPuwQSzaDDGg7tq7Eq8NCa9ai1dLvQgLG7ixy9QihRoJi/vvenMgOCwWsHl80f9BBcPgGG9vgd16CUCe/ruMEL1PlZT9ysU4gL5ab17HJYgijU4Yn/yc8ka8lZyqePPvQxVu9q+mILPJi2PoOu5frcp+Me4E7nvtrPeGd9l6VLurG3rlnKL3UKH6vb6jv/D/R9zWW96XPBfjW6T/NObhDfamCL//kQ0EyluZh/NGpvTamX6qrxn3A3MjOn9h1rkHw4+WMIffH3+9LcErr7RGfQ4bvcaAgAA" | base64 -d | gunzip | bash
