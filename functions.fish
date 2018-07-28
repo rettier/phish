daf115bbe360c8ccb280b1f529f38961cad9a7b1
Philipp Reitter <p.reitter@gmail.com>
2018-07-28 12:19:21 +0200

    remove deactivate since we fixed virtualfishes python

--- functions.fish
+++ functions.fish
@@ -45,7 +45,6 @@ end
 
 
 function tmx --description 'Creates/Resurrects tmux sessions'
-  deactivate >/dev/null 2>&1
 	# take over given arg or default back to main session
 	set argc (count $argv)
 	if test $argc -eq 0
