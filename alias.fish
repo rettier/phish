8ee186d740d4f48a71ad6e456cb3b371eafc1e3a
Philipp Reitter <i@philipp.ninja>
2020-05-10 13:47:01 +0200

    cleaned up some unused abbr, added some mor for kubectl

--- alias.fish
+++ alias.fish
@@ -5,7 +5,6 @@ abbr -a nano vim
 alias ccat='command cat'
 alias meh='echo "¯\\_(ツ)_/¯"'
 alias pubip='curl ipecho.net/plain; and echo'
-alias htf='fish_logo; and echo "       All hail the Fish"'
 alias make="make -j(nproc --all)"
 abbr -a unixc unix \(c\)
 abbr -a ssh-agent eval \(ssh-agent -c\)
@@ -35,6 +34,8 @@ abbr -a pirr pip install -r requirements.txt
 abbr -a pm python manage.py
 abbr -a pmm python manage.py migrate 
 abbr -a pmmk python manage.py makemigrations
+abbr -a pmmke python manage.py makemessages
+abbr -a pmcme python manage.py compilemessages
 abbr -a pmdd python manage.py devdata
 abbr -a pmsp python manage.py shell_plus
 abbr -a pms python manage.py shell
@@ -45,25 +46,20 @@ abbr -a dc docker-compose
 abbr -a dcud docker-compose up -d
 
 # json
-alias j='python -c "import sys, json; print json.dumps(json.load(sys.stdin), sort_keys=True, indent=2)" | cat'
 alias pj='python -c "import sys, ast, json; print json.dumps(ast.literal_eval(sys.stdin.read().strip()))" | j'
-alias cj='c | j'
+alias cj='c | jq'
 
 # tmux
 alias t='tmx'
-alias res='cp $path/tmux/resurrect/last ~/.tmux/resurrect/last'
-
-# ansible
-alias play='ansible-playbook'
-alias playask='ansible-playbook --ask-become-pass'
-alias playsudo='ansible-playbook --extra-vars 'ansible_ssh_user=root''
 
 # kubernetes helpers
 alias kname='kubectl config set-context (kubectl config current-context) --namespace '
-abbr -a kaf kubectl apply -f
-abbr -a k   kubectl
+abbr -a kaf   kubectl apply -f
+abbr -a k     kubectl
 abbr -a kdp   kubectl delete pod
 abbr -a kgp   kubectl get pods
+abbr -a ktp   kubectl top pod
+abbr -a ktn   kubectl top node
 
 abbr -a r redis-cli
 
